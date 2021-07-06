7720ae20823b30a4db0e666208865b5dab7cbd7e
Sergey Golovanov <golovanov@ujin.tech>
2021-07-06 20:26:02 +0500

    UNIT_TEST - add RouterTest()

--- autoload.php
+++ autoload.php
@@ -5,6 +5,6 @@ use microservice_template\coreapp\AutoloadNamespace;
 require_once __DIR__ . '/coreapp/AutoloadNamespace.php';
 
 $autoloader = new AutoloadNamespace();
-$autoloader->addNamespace('archive', __DIR__);
+$autoloader->addNamespace('microservice_template', __DIR__);
 
 $autoloader->register();
